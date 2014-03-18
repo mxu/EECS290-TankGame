35a7af45b6ad1aeea9f419857622d71355ad303b
jrf121 <jrf121@case.edu>
2014-03-18 15:58:06 -0400

    I added the tank
    Doesn't work too well. The tank goes and turns and sucks.

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public Vector3 torque = new Vector3(0f, 100f, 0f);
+	public Rigidbody[] leftWheels;
+	public Rigidbody[] rightWheels;
+	public Rigidbody turret;
+	
+	void Start() {
+	}
+	
+	
+	void Update () {
+	
+		if (Input.GetKey (KeyCode.W)) {
+			this.moveForward();
+		}
+		if (Input.GetKey (KeyCode.S)) {
+			this.moveBackward();
+		}
+		if (Input.GetKey (KeyCode.A)) {
+			this.turnLeft();
+		}
+		if (Input.GetKey (KeyCode.D)) {
+			this.turnRight();
+		}
+		
+		
+	}
+	
+	private void moveForward() {
+		foreach (Rigidbody rb in leftWheels){
+			rb.AddRelativeTorque(-torque);
+		}
+		foreach (Rigidbody rb in rightWheels){
+			rb.AddRelativeTorque(-torque);
+		}
+	}
+	
+	public void moveBackward(){
+		foreach (Rigidbody rb in leftWheels){
+			rb.AddRelativeTorque(torque);
+		}
+		foreach (Rigidbody rb in rightWheels){
+			rb.AddRelativeTorque(torque);
+		}
+	}
+	
+	public void turnRight(){
+		foreach (Rigidbody rb in leftWheels){
+			rb.AddRelativeTorque(-torque);
+		}
+		foreach (Rigidbody rb in rightWheels){
+			rb.AddRelativeTorque(torque);
+		}
+	}
+	
+	public void turnLeft(){
+		foreach (Rigidbody rb in leftWheels){
+			rb.AddRelativeTorque(torque);
+		}
+		foreach (Rigidbody rb in rightWheels){
+			rb.AddRelativeTorque(-torque);
+		}
+	}
+	
+}
