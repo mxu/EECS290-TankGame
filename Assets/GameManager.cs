c2cac2ee96ee8bbd964eff4f623e5abfcb3c9eac
andrewMarmorstein <agm67@case.edu>
2014-03-20 15:46:47 -0400

    map updates
    added wall texture and and game/spawn managers and added a skybox.

---
+++ Assets/GameManager.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+	public delegate void GameEvent();
+
+	//Keeps track of number of enemies
+	private static int numberOfEnemies;
+	//Keeps track of number of allies
+	private static int numberOfAllies;
+
+	public void Start() {
+		numberOfEnemies = 3;
+		numberOfAllies = 3;
+	}
+
+	//Called when enemy is destroyed.
+	public static void DecrementEnemyCount() {
+		if(numberOfEnemies>1){
+			numberOfEnemies--;
+		}
+		else if (numberOfEnemies == 0){
+			//Call gameover screen
+		}
+
+	}
+
+	//Called when ally is destroyed
+	public static void DecrementAllyCount() {
+		if(numberOfAllies >1){
+			numberOfAllies--;
+		}
+		else if (numberOfAllies == 0){
+			//Call gameover screen
+		}
+	}
+	
+
+
+
+}
---
+++ Assets/map managers/SpawnManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnManager : MonoBehaviour {
+
+	//Good side
+	public GameObject cornerSpawn;
+	public GameObject cornerSpawnRight;
+	public GameObject cornerSpawnLeft;
+
+	//Bad side
+	public GameObject enemyCornerSpawn;
+	public GameObject enemyCornerSpawnRight;
+	public GameObject enemyCornerSpawnLeft;
+
+
+	// Use this for initialization
+	void Start () {
+
+		//Array created for easy organizing and calling of spawn points.
+		GameObject[] arrayOfSpawnPoints= {cornerSpawn, cornerSpawnLeft, cornerSpawnRight};
+		GameObject[] arrayOfEnemySpawnPoints = {enemyCornerSpawn, enemyCornerSpawnRight, enemyCornerSpawnLeft};
+
+		
+	}
+	
+}
