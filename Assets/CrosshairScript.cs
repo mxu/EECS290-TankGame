4662396cd72326c406920b42983e917536350ba3
whh34 <whh34@case.edu>
2014-03-21 14:50:28 -0400

    Camera Movement! Version 2! NOW WITH VERTICAL SUPPORT
    YOUR GOD IS HERE. JUST MOVE THE MOUSE.

---
+++ Assets/CrosshairScript.cs
@@ -0,0 +1,24 @@
+﻿// DIS SHIT CRAY
+// DONT EVEN WARGARBLE ALL OVER IT
+using UnityEngine;
+using System.Collections;
+
+public class CrosshairScript : MonoBehaviour {
+
+	GameObject shootSpot;
+	GameObject turret;
+	Vector3 initialPos;
+
+	// Use this for initialization
+	void Start () {
+		shootSpot = GameObject.Find("ShootSpot");
+		turret = GameObject.Find ("PlayerTurret");
+		initialPos = this.transform.position - turret.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.localPosition = (shootSpot.transform.position - turret.transform.position) * 2;
+		this.transform.localRotation = shootSpot.transform.localRotation;
+	}
+}
--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -15,7 +15,7 @@ public class PlayerController : MonoBehaviour {
 	
 	void Update () {
 	
-		this.rotateCamera(Input.GetAxis("Horizontal"));
+		this.rotateCamera(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
 	
 		if (Input.GetKey (KeyCode.W)) {
 			tankController.moveForward();
@@ -33,13 +33,21 @@ public class PlayerController : MonoBehaviour {
 			tankController.shoot();
 		}
 		
-		tankController.pointGunAt(currentCameraRotation);
+		tankController.pointGunAt(new Vector3(-currentCameraRotation.x + 55, currentCameraRotation.y, 0f));
 	}
 	
-	// This helper method changes the camera position to follow with mouse input
-	public void rotateCamera (float relativeMS) {
-		Vector3 targetRotation = new Vector3(0f, relativeMS, 0f);
-		cameraRotater.transform.localRotation = Quaternion.Euler(targetRotation + currentCameraRotation);
+	/* This helper method changes the camera position to follow with mouse input
+	 * @param relativeMSX The relative horizontal mouse speed at a given frame
+	 * @param relativeMSY The relative vertical mouse speed at a given frame */
+	public void rotateCamera (float relativeMSX, float relativeMSY) {
+		Vector3 targetRotation = new Vector3(relativeMSY, relativeMSX, 0f); // Takes the mouse motion and translates it to rotation angles for the camera
+		Vector3 temp  = targetRotation + currentCameraRotation;
+		float boundary =  temp.x;
+		if (boundary < -5) 
+			boundary = -5; 
+		if (boundary > 25)
+			boundary = 25;
+		cameraRotater.transform.localRotation = Quaternion.Euler(boundary, temp.y, 0f);
 		currentCameraRotation = targetRotation + currentCameraRotation;
 	}
 }
