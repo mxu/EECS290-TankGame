d649634746fa41a47b0c8cf4a79a6cf3ae38d4cd
jrf121 <jrf121@case.edu>
2014-03-21 14:38:24 -0400

    Made Some Explosions
    The bullets explode and have a contrail

---
+++ Assets/BulletBehavior.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletBehavior : MonoBehaviour {
+
+	public ParticleSystem emit;
+	public GameObject explosion;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	public void OnCollisionEnter(){
+		this.Explode();
+		emit.transform.parent = null;
+		emit.emissionRate = 0;
+		emit.GetComponent<TimedObjectDestroy>().initiated = true;
+		Destroy(this.gameObject);
+	}
+	
+	public void Explode(){
+		GameObject boom = (GameObject) GameObject.Instantiate(explosion);
+		boom.transform.position = this.transform.position;
+	}
+}
---
+++ Assets/TimedObjectDestroy.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimedObjectDestroy : MonoBehaviour {
+	public float time;
+	private float timeCounter;
+	public bool initiated;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+
+	
+	// Update is called once per frame
+	void Update () {
+		if (initiated){
+			if (timeCounter > time)
+				Destroy(gameObject);
+			timeCounter += Time.deltaTime;
+		}
+				
+	}
+}
